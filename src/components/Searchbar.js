ac2290b27d41b6febfe0ffaf729eeda912e9e189
Ritik <ritikpatel82993@gmail.com>
2024-11-27 14:37:37 +0530

    first commit

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,28 +1,58 @@
-
-
-
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { setQuery, addSearchHistory } from '../store';
-
 import { IoSearch } from "react-icons/io5"; 
-
 
 export default function SearchBar() {
   const dispatch = useDispatch();
 
   const handleSearch = (e) => {
     if (e.key === 'Enter') {
-      dispatch(setQuery(e.target.value));
-      dispatch(addSearchHistory({ query: e.target.value, time: new Date() }));
+      const searchQuery = e.target.value.trim();
+      if (searchQuery) {
+        dispatch(setQuery(searchQuery));
+        dispatch(addSearchHistory({ query: searchQuery, time: new Date() }));
+      }
     }
   };
 
   return (
     <div className="search-input-container search-bar">
-    <span className="search-icon">< IoSearch /></span>
-      <input type="text" placeholder="Search stories by title, author, or URL" className='search-input' onKeyPress={handleSearch} />
-      
+      <span className="search-icon"><IoSearch /></span>
+      <input
+        type="text"
+        placeholder="Search stories by title, author, or URL"
+        className="search-input"
+        onKeyPress={handleSearch}
+      />
     </div>
   );
 }
+
+
+
+// import React from 'react';
+// import { useDispatch } from 'react-redux';
+// import { setQuery, addSearchHistory } from '../store';
+
+// import { IoSearch } from "react-icons/io5"; 
+
+
+// export default function SearchBar() {
+//   const dispatch = useDispatch();
+
+//   const handleSearch = (e) => {
+//     if (e.key === 'Enter') {
+//       dispatch(setQuery(e.target.value));
+//       dispatch(addSearchHistory({ query: e.target.value, time: new Date() }));
+//     }
+//   };
+
+//   return (
+//     <div className="search-input-container search-bar">
+//     <span className="search-icon">< IoSearch /></span>
+//       <input type="text" placeholder="Search stories by title, author, or URL" className='search-input' onKeyPress={handleSearch} />
+      
+//     </div>
+//   );
+// }
